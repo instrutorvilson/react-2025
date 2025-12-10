d7873d7068f5fdbb0caedfb6593fcfd5db2f69be
instrutorvilson <instrutorvilsonmoro@gmail.com>
2025-12-10 19:25:16 -0300

    remover indesejados

--- my-react-app/src/componentes/carro.tsx
+++
@@ -1,7 +0,0 @@
-function Car(props:any) {
-  const {placa} = props
-  return (
-    <h2>Hi, I am a Car with board: {placa}</h2>
-  );
-}
-export default Car
--- my-react-app/src/componentes/carrocas.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Carroca extends React.Component {
-  render() {
-    return <h2>Hi, I am a Carroça!</h2>;
-  }
-}
-
-export default Carroca
