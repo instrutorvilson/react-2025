074c057a0356ca9872f7315ba28857f2868613a4
instrutorvilson <instrutorvilsonmoro@gmail.com>
2026-01-14 19:03:15 -0300

    persistencia com asyncstorage

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
+  const [nome, setNome] = useState('')
+
+  useEffect(() => {
+    carregarNome();
+  }, []);
+
+  async function salvarNome() {
+    await AsyncStorage.setItem('nome', nome);
+  }
+
+  async function carregarNome() {
+    const nomeSalvo = await AsyncStorage.getItem('nome');
+    nomeSalvo ? setNome(nomeSalvo) : setNome('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{marginVertical: 10}}>Nome salvo: {nome}</Text>
+
+      <TextInput 
+        placeholder="EX: ana"
+        onChangeText={setNome}
+        value={nome}
+        style={styles.input}              
+      />
+      <Button title="Salvar nome" onPress={salvarNome} />
+      
+      <Button title="limpar nome" onPress={async() => await AsyncStorage.removeItem('nome')} />
+
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20  
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#999',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5
+  }
 });
